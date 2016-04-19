03ed72bd7b5b70d6871267d24905f80a9e4b9b2c
noppoman <yuki@miketokyo.com>
2016-04-20 05:52:49 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # MustacheViewEngine
+
+Mustache render engine for Slimane
+
+## Usage
+
+```swift
+import Slimane
+
+let app = Slimane()
+
+app.get("/") { req, responder in
+  responder {
+      let render = Render(engine: MustacheViewEngine(templateData: ["name": "Slimane"]), path: "index")
+      Response(custome: render)
+  }
+}
+```
---
+++ Sources/MustacheViewEngine.swift
@@ -0,0 +1,45 @@
+//
+//  Render.swift
+//  Render
+//
+//  Created by Yuki Takei on 4/20/16.
+//
+//
+
+@_exported import C7
+@_exported import Renderable
+@_exported import Mustache
+@_exported import Suv
+
+public typealias TemplateData = MustacheBoxable
+
+public struct MustacheViewEngine: Renderable {
+    public var fileExtension = "mustache"
+    public let templateData: TemplateData
+
+    public init(templateData: TemplateData){
+        self.templateData = templateData
+    }
+
+    public func render(path: String, result: (Void throws -> Data) -> Void) {
+        FS.readFile("\(path).\(fileExtension)") {
+            if case .Error(let err) = $0 {
+                result {
+                    throw err
+                }
+            } else if case .Success(let buf) = $0 {
+                do {
+                    let template = try Template(string: buf.toString()!)
+                    let compiled = try template.render(Box(boxable: self.templateData))
+                    result {
+                        Data(compiled)
+                    }
+                } catch {
+                    result {
+                        throw error
+                    }
+                }
+            }
+        }
+    }
+}

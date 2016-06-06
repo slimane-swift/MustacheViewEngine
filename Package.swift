d33abe4ee87c8a044756bab2774719d5c533d2fd
noppoman <yuki@miketokyo.com>
2016-06-07 01:20:52 +0900

    Bump Suv version to 0.6

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/Zewo/Mustache.git", majorVersion: 0, minor: 6),
         .Package(url: "https://github.com/slimane-swift/Renderable.git", majorVersion: 0, minor: 3),
-        .Package(url: "https://github.com/noppoMan/Suv.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/noppoMan/Suv.git", majorVersion: 0, minor: 6),
     ]
 )
